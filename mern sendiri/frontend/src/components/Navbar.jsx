4bbb92ef5c2eea74bbe1f208ad9c60c4e2bed8b7
lamdarv <lamda.richo.tif420@polban.ac.id>
2022-05-31 15:49:42 +0700

    First Commit

---
+++ mern sendiri/frontend/src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+const Navbar = () => {
+  return (
+    <div className='n-container'>
+        <div className="n-row">
+            <div className="n-col">
+                <span className='n-email'>lamdarvs@gmail.com
+                </span>
+            </div>
+            <div className="n-col">
+                <div className="socials">
+                    <FontAwesomeIcon icon={} />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
